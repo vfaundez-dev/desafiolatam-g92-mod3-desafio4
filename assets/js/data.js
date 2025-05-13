023a8c0bfbdc1a4287ec5e06f085c5670016a6f0
Vladimir Faundez Hernandez <v.faundezh@gmail.com>
2025-05-13 15:33:19 -0400

    Agregado index.js y data.js. Incluido arreglos para venta y arriendo

---
+++ assets/js/data.js
@@ -0,0 +1,115 @@
+export const propiedades_venta = [
+  {
+    nombre: 'Apartamento de lujo en zona exclusiva',
+    src: 'https://fotos.perfil.com/2018/09/21/trim/950/534/nueva-york-09212018-366965.jpg',
+    descripcion: 'Este apartamento de lujo está ubicado en una exclusiva zona residencial',
+    ubicacion: '123 Luxury Lane, Prestige Suburb, CA 45678',
+    habitaciones: 4,
+    banios: 3,
+    costo: 5000,
+    smoke:false,
+    pets: false
+  },
+  {
+    nombre: '',
+    src: '',
+    descripcion: '',
+    ubicacion: '',
+    habitaciones: 0,
+    banios: 3,
+    costo: 0,
+    smoke: false,
+    pets: true
+  },
+  {
+    nombre: '',
+    src: '',
+    descripcion: '',
+    ubicacion: '',
+    habitaciones: 0,
+    banios: 3,
+    costo: 0,
+    smoke: false,
+    pets: true
+  },
+  {
+    nombre: '',
+    src: '',
+    descripcion: '',
+    ubicacion: '',
+    habitaciones: 0,
+    banios: 3,
+    costo: 0,
+    smoke: false,
+    pets: true
+  },
+  {
+    nombre: '',
+    src: '',
+    descripcion: '',
+    ubicacion: '',
+    habitaciones: 0,
+    banios: 3,
+    costo: 0,
+    smoke: false,
+    pets: true
+  }
+];
+
+export const propiedades_arriendo = [
+  {
+    nombre: '',
+    src: '',
+    descripcion: '',
+    ubicacion: '',
+    habitaciones: 0,
+    banios: 0,
+    costo: 0,
+    smoke: false,
+    pets: true
+  },
+    {
+    nombre: '',
+    src: '',
+    descripcion: '',
+    ubicacion: '',
+    habitaciones: 0,
+    banios: 3,
+    costo: 0,
+    smoke: false,
+    pets: true
+  },
+  {
+    nombre: '',
+    src: '',
+    descripcion: '',
+    ubicacion: '',
+    habitaciones: 0,
+    banios: 3,
+    costo: 0,
+    smoke: false,
+    pets: true
+  },
+  {
+    nombre: '',
+    src: '',
+    descripcion: '',
+    ubicacion: '',
+    habitaciones: 0,
+    banios: 3,
+    costo: 0,
+    smoke: false,
+    pets: true
+  },
+  {
+    nombre: '',
+    src: '',
+    descripcion: '',
+    ubicacion: '',
+    habitaciones: 0,
+    banios: 3,
+    costo: 0,
+    smoke: false,
+    pets: true
+  }
+];
---
+++ assets/js/index.js
@@ -0,0 +1,4 @@
+import { propiedades_venta, propiedades_arriendo } from './data.js';
+
+
+console.log(propiedades_venta)
