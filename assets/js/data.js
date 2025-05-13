b8f75452c212aa1efd4b847043a6e0d7adbcea59
Vladimir Faundez Hernandez <v.faundezh@gmail.com>
2025-05-13 16:08:01 -0400

    Agregada imagenes y completado array para seccion venta

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,106 +1,62 @@
 export const propiedades_venta = [
   {
-    nombre: 'Apartamento de lujo en zona exclusiva',
-    src: 'https://fotos.perfil.com/2018/09/21/trim/950/534/nueva-york-09212018-366965.jpg',
-    descripcion: 'Este apartamento de lujo está ubicado en una exclusiva zona residencial',
-    ubicacion: '123 Luxury Lane, Prestige Suburb, CA 45678',
+    nombre: 'Casa contemporanea en barrio exclusivo',
+    src: './assets/img/casa_venta1.jpg',
+    descripcion: 'Hermosa casa moderna, ubicada en un barrio exclusivo alejado de la chusma, cuenta con amplio estacionamiento y sistema domótico completo.',
+    ubicacion: 'Nido de Aguilas 2244, Las Tranqueras, CA 12345',
     habitaciones: 4,
-    banios: 3,
-    costo: 5000,
-    smoke:false,
+    banios: 2,
+    costo: 240000,
+    smoke: true,
+    pets: true
+  },
+    {
+    nombre: 'Casa prefabricada',
+    src: './assets/img/casa_venta2.jpg',
+    descripcion: 'Casa construida principalmente de pino de la Patagonia, cuenta con un sistema de calefacción, agua y luz. Ideal para vivir en la montaña.',
+    ubicacion: 'Camino el Roble 5432, La Hectarea, CA 55673',
+    habitaciones: 3,
+    banios: 1,
+    costo: 120000,
+    smoke: false,
+    pets: true
+  },
+  {
+    nombre: 'Casa de campo',
+    src: './assets/img/casa_venta3.jpg',
+    descripcion: 'Hermosa casa de campo, ubicada en un lugar tranquilo y alejado de la ciudad. Ideal para disfrutar de la naturaleza y ver como las vacas comen.',
+    ubicacion: 'La Rupertina 8756, Camino de Barro, CA 78901',
+    habitaciones: 20,
+    banios: 4,
+    costo: 200000,
+    smoke: true,
+    pets: true
+  },
+  {
+    nombre: 'Caja de Fosforos',
+    src: './assets/img/casa_venta3.jpg',
+    descripcion: 'Pequeña casa, aislada del mundo, ideal para antisociales o aquellos que no quieran contacto con el mundo exterior. Cuenta con espacio para usted y su amigo imaginario.',
+    ubicacion: '',
+    habitaciones: 2,
+    banios: 1,
+    costo: 80000,
+    smoke: false,
     pets: false
   },
   {
-    nombre: '',
-    src: '',
-    descripcion: '',
-    ubicacion: '',
-    habitaciones: 0,
-    banios: 3,
-    costo: 0,
-    smoke: false,
-    pets: true
-  },
-  {
-    nombre: '',
-    src: '',
-    descripcion: '',
-    ubicacion: '',
-    habitaciones: 0,
-    banios: 3,
-    costo: 0,
-    smoke: false,
-    pets: true
-  },
-  {
-    nombre: '',
-    src: '',
-    descripcion: '',
-    ubicacion: '',
-    habitaciones: 0,
-    banios: 3,
-    costo: 0,
-    smoke: false,
-    pets: true
-  },
-  {
-    nombre: '',
-    src: '',
-    descripcion: '',
-    ubicacion: '',
-    habitaciones: 0,
-    banios: 3,
-    costo: 0,
-    smoke: false,
-    pets: true
+    nombre: 'Casa Embrujada',
+    src: './assets/img/casa_venta3.jpg',
+    descripcion: 'Casa para realizar rituales oscuros, cuenta con un sótano y una habitación secreta. Ideal para aquellos que buscan hacer peliculas de terror de bajo presupuesto o rituales satánicos.',
+    ubicacion: 'Katulu 666, La Isla de los Muertos, CA 66666',
+    habitaciones: 4,
+    banios: 1,
+    costo: 70000,
+    smoke: true,
+    pets: false
   }
 ];
 
 export const propiedades_arriendo = [
-  {
-    nombre: '',
-    src: '',
-    descripcion: '',
-    ubicacion: '',
-    habitaciones: 0,
-    banios: 0,
-    costo: 0,
-    smoke: false,
-    pets: true
-  },
-    {
-    nombre: '',
-    src: '',
-    descripcion: '',
-    ubicacion: '',
-    habitaciones: 0,
-    banios: 3,
-    costo: 0,
-    smoke: false,
-    pets: true
-  },
-  {
-    nombre: '',
-    src: '',
-    descripcion: '',
-    ubicacion: '',
-    habitaciones: 0,
-    banios: 3,
-    costo: 0,
-    smoke: false,
-    pets: true
-  },
-  {
-    nombre: '',
-    src: '',
-    descripcion: '',
-    ubicacion: '',
-    habitaciones: 0,
-    banios: 3,
-    costo: 0,
-    smoke: false,
-    pets: true
-  },
   {
     nombre: '',
     src: '',
